0c4bd5a0a57b5df60a087f313449eb660c20f49a
MiyuFujitsuka <shiraduki.94@gmail.com>
2023-08-11 14:40:33 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/index'
-  get 'books/show'
   get 'books/edit'
-  get '/top' => 'homes#top'
-  resources :books
+  get '/' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books'=>'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  #resources :books
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
